93c314e0897b1f682154b002e9c2602166d4c47f
Joe Spraggon <joe_spraggon@hotmail.co.uk>
2021-01-18 16:59:39 +0000

    playing with params

--- app.rb
+++ app.rb
@@ -13,7 +13,14 @@ get '/secret' do
   "What are you doing here?"
 end
 
-get '/cat' do
+get '/random-cat' do
   @cat_name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @cat_name = params[:name]
+  @cat_age = params[:age]
   erb(:index)
 end
